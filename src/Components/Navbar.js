7f217c0e2f87e4d1e265d9115dd7fd6fcf36c9fa
Nadir Ahmed <qx.nadir@gmail.com>
2022-12-24 00:16:56 +0600

    done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = () => {
     return (
         <div className='navbar  fixed w-full top-0  '>
             <div className=' max-w-7xl mx-auto flex justify-between items-center h-20'>
-                <div>
+                <div className='pl-5'>
                     <h1 className='logo'>N</h1>
                 </div>
                 <div className='hidden md:flex items-center justify-center gap-4'>
